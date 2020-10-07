5287d32ddc51c3c19f3da2411e9199ee515e54b2
Elena Michelle Gonzalez <al221910360@gmail.com>
2020-10-06 22:55:23 -0500

    talas de multiplicar

---
+++ tablas.dart
@@ -0,0 +1,15 @@
+void main() {
+  int tabla, por;
+  for (tabla = 1; tabla <= 10; tabla++) {
+    // print("\nTabla del " + tabla.toString());
+    print("\nTabla del $tabla");
+    for (por = 1; por <= 10; por++) {
+      //print(tabla.toString() +
+      //    '*' +
+      //    por.toString() +
+      //    '=' +
+      //    (tabla * por).toString());
+      print('$tabla * $por= ${tabla * por}');
+    }
+  }
+}
