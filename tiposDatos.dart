2bcd7a23d10f43a8ded87d9e1d78b5bc3677d675
Elena Michelle Gonzalez <al221910360@gmail.com>
2020-10-06 22:55:52 -0500

    tipos de datos

---
+++ tiposDatos.dart
@@ -0,0 +1,25 @@
+void main() {
+  // NO Recomendable
+  // var name = 'Voyager I';
+  // var year = 1977;
+  // var antennaDiameter = 3.7;
+  // var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  // var image = {
+  //   'tags': ['saturn'],
+  //   'url': '//path/to/saturn.jpg'
+  // };
+  String name = 'Voyager I';
+  int year = 1977;
+  double antennaDiameter = 3.7;
+  List flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  Map image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print(name);
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects);
+  print(image);
+}
