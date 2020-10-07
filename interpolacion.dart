1d06d7164f8076edebb82788cdf309df9e66d25a
Elena Michelle Gonzalez <al221910360@gmail.com>
2020-10-06 22:54:12 -0500

    interpolacion

---
+++ interpolacion.dart
@@ -0,0 +1,8 @@
+void main() {
+  String nombre = 'Miguel Rivera Robles';
+  String saludo = 'Hola';
+  int edad = 23;
+  print(
+      '$saludo; $nombre, ¿Tienes $edad años?, tu nombre tiene: ${nombre.length} caracteres');
+  print('Tu nombre en mayusculas es: ${nombre.toUpperCase()}');
+}
