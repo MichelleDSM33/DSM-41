661b80acd428c5391d3e421c5e15e935a26b6944
Elena Michelle Gonzalez <al221910360@gmail.com>
2020-10-06 21:38:19 -0500

    clases

---
+++ clases.dart
@@ -0,0 +1,30 @@
+class Persona {
+  String nombre;
+  String apellido;
+}
+
+Run | Debug
+void main() {
+  conts double PI=3.14 //constante
+  Persona benito = new Persona();
+  juan = new persona();
+  final Persona  benito = new Perosna(); //Instalancia,objecto 
+}
+
+class Ventana{}
+   
+class BarraDeDireccion{}
+      
+class Menu {}
+
+class ElementoMenu{
+  String texto;
+  String metodoAbreviado;
+  int colorPrincipal;
+  int colorFondoActvo;
+  int colorFondoInactivo;
+  bool hablitado;
+ 
+   void az
+}
+   
